3df59f60395291e394ebec9d1a3c7cedca8ecd84
RahulDeMaverick <udhayakumar.r@northeastern.edu>
2023-05-15 23:38:31 -0400

    3

--- src/main/java/com/example/demo/repository/PatientRepository.java
+++ src/main/java/com/example/demo/repository/PatientRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.pojo.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+//repsitory
 
 @Repository
 public interface PatientRepository extends JpaRepository<Patient, Long> {
